348bdffb690209959c78bd9d40d5da3e314af0bc
Timofei <timofei.dev.tregubov@gmail.com>
2024-03-22 19:33:41 +0700

    update token

--- src/redux/api.tsx
+++ src/redux/api.tsx
@@ -2,8 +2,9 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import {GithubEvent} from '../shared.types';
 
 const baseUrl = 'https://api.github.com/';
+// in real project use secure store
 const token =
-  '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';
+  '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';
 
 export const api = createApi({
   reducerPath: 'api',
